8372fe88dec782ec7472c85b7780270ab2b395b4
Allan-SS <allansousasilva1@gmail.com>
2021-04-27 16:30:40 -0300

    Realizando a organização do projeto

---
+++ semana11/labEddit/src/routes/Router.js
@@ -0,0 +1,27 @@
+import React  from 'react'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+
+const Router = () => {
+    return(
+        <BrowserRouter>
+        <Switch>
+            <Router>
+
+            </Router>
+
+                <Router>
+
+                </Router>
+
+                <Router>
+
+                </Router>
+
+                <Router>
+
+                </Router>
+        </Switch>
+        </BrowserRouter>
+    )
+}
