b05297e8ef50bb0c0afe971bf19f8430b4ce8fcc
Allan-SS <allansousasilva1@gmail.com>
2021-05-17 17:10:59 -0300

    Realização Dos Primeiros Exercicios De Back

---
+++ semana15/Node-package/Exercicio3/exercicio3.js
@@ -0,0 +1,7 @@
+//Exercicio 3
+
+const list = process.argv[2]
+const newList = ["Limpar a Casa", "Arrumar o Quarto"]
+
+newList.push(list)
+console.log(newList)
---
+++ semana15/Node-package/Exercicio4/exercicio4.js
@@ -0,0 +1,49 @@
+// Exercício 1
+
+//A - atraves do terminal ou no package utilizando o codigo "node ./***.js"
+
+//B -
+
+// const name = process.argv[2]
+// const age = Number(process.argv[3])
+
+// console.log(`Olá, ${name}! Você tem ${age} anos.`)
+
+//C -
+
+// const newAge = age + 7
+
+// console.log(`Olá, ${name}! Você tem ${age} anos. Em sete anos você terá ${newAge}`)
+
+// Exercício 2
+
+// const operaçao = process.argv[2]
+// const arg1 = Number(process.argv[3])
+// const arg2 = Number(process.argv[4])
+
+// switch(operaçao) {
+//     case "add":
+//         const add = arg1 + arg2
+//         console.log(`Resposta: ${add} `)
+//     break;
+//     case "sub":
+//         const sub = arg1 - arg2
+//         console.log(`Resposta: ${sub} `)
+//     break;
+//     case "mult":
+//         const mult = arg1 * arg2
+//         console.log(`Resposta: ${mult} `)
+//     break;
+//     case "div":
+//         const div = arg1 / arg2
+//         console.log(`Resposta: ${div} `)
+//     break;
+// }
+
+//Exercicio 3
+
+// const list = process.argv[2]
+// const newList = ["Limpar a Casa", "Arrumar o Quarto"]
+
+// newList.push(list)
+// console.log(newList)
