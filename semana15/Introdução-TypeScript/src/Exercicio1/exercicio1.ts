1d5e9c838c91a047d969dfed0e9ee304c9a226d6
Allan-SS <allansousasilva1@gmail.com>
2021-05-18 16:58:50 -0300

    Ainda Realizando parte do exercicios

---
+++ semana15/Introdução-TypeScript/src/Exercicio1/exercicio1.ts
@@ -0,0 +1,40 @@
+//Exercicio 1
+
+
+//A -
+
+// let minhaString: string = 2
+
+//O codigo quebrou acusando estar usando um number ao inves de uma string
+
+//B - 
+
+// let meuNumero: number = "Hello! World"
+
+//O codigo quebrou acusando estar usando uma string ao inves de uma number
+
+//C -
+
+type pessoa = {
+    nome: string,
+    idade: number,
+    corFavorita: string
+}
+
+const joao: pessoa = {
+    nome: "Joao Alves",
+    idade: 28,
+    corFavorita: "laranja"
+}
+
+const paulo: pessoa = {
+    nome: "Paulo Otavio",
+    idade: 22,
+    corFavorita: "azul"
+}
+
+const allan: pessoa = {
+    nome: "Allan Silva",
+    idade: 18,
+    corFavorita: "vermelho"
+}
